9a52a93e23adfe3ad934e8d19cb124870e00e8d5
Kai Gehry <kaigehry@gmail.com>
2025-03-25 20:10:42 -0700

    File templates added to project and muptiple linked lists can now be created and used in a silgle file.

--- Data_Strutures/Singly_Linked_List.h
+++ Data_Strutures/Singly_Linked_List.h
@@ -3,56 +3,61 @@
 #include <stdbool.h>
 #include "Singly_Linked_List_Node.h"
 
-// struct Singly_Linked_List
-// {
-// };
+typedef struct
+{
+    struct Singly_Linked_List_Node head;
+    struct Singly_Linked_List_Node *current;
+    int list_size;
+} Singly_Linked_List;
 
-// Struct which defines the node at the head of the linked list
-struct Singly_Linked_List_Node head;
-// Struct which defines the node at the end of the linked list (set as null)
-struct Singly_Linked_List_Node *current;
-// Holds the value of the list size
-int list_size;
+void initialize_list(Singly_Linked_List *list)
+{
+    list->list_size = 0;
+}
 
 // Prints the linked list to the terminal
-void display_linked_list()
+void display_linked_list(Singly_Linked_List *list)
 {
-    struct Singly_Linked_List_Node node = head;
-    printf("\n");
 
-    while (node.next != NULL)
+    if (list->list_size != 0)
     {
-        printf("%d->", (node).value);
-        node = *(node.next);
+        struct Singly_Linked_List_Node node = list->head;
+        printf("\n");
+
+        while (node.next != NULL)
+        {
+            printf("%d->", (node).value);
+            node = *(node.next);
+        }
     }
 
     printf("NULL");
 }
 
 // Returns the element at the head of the linked list
-int return_head_element()
+int return_head_element(Singly_Linked_List *list)
 {
-    return head.value;
+    return (list->head).value;
 }
 
 // Returns the length of the linked list
-int list_length()
+int list_length(Singly_Linked_List *list)
 {
-    return list_size;
+    return list->list_size;
 }
 
 // Adds a node to the linked list
-void add_node(int value)
+void add_node(Singly_Linked_List *list, int value)
 {
     // Linked list node pointer which represents the new node to be added
     struct Singly_Linked_List_Node *new_node = (struct Singly_Linked_List_Node *)malloc(sizeof(struct Singly_Linked_List_Node));
     // Linked list node pointer which represents the next node the newly added node points to
     struct Singly_Linked_List_Node *next_node = (struct Singly_Linked_List_Node *)malloc(sizeof(struct Singly_Linked_List_Node));
 
-    if (list_size != 0)
+    if (list->list_size != 0)
     {
         // Sets new node to point to the address of the node at the end of the list
-        new_node = current;
+        new_node = list->current;
         // Sets the value of the new node
         (*new_node).value = value;
         // Points to the node which will now be at the end of the list
@@ -60,27 +65,27 @@ void add_node(int value)
     }
 
     // Sets the node which previously pointed to the end of the list to the new node at the end of the list
-    current = next_node;
+    list->current = next_node;
     // Sets the end of the list's next pointer to NULL
-    (*current).next = NULL;
+    (*(list->current)).next = NULL;
 
     // Defines the head of the linked list
-    if (list_size == 0)
+    if (list->list_size == 0)
     {
-        head.value = value;
-        head.next = current;
+        (list->head).value = value;
+        (list->head).next = list->current;
     }
 
-    list_size++;
+    list->list_size++;
 }
 
 // Removes a node from the linked list
-void remove_node(int value)
+void remove_node(Singly_Linked_List *list, int value)
 {
     // Place holder node used to hold the nodes as the list is traversed
-    struct Singly_Linked_List_Node search_node = head;
+    struct Singly_Linked_List_Node search_node = list->head;
     // Pointer used to keep a reference to the previous node in the traversal
-    struct Singly_Linked_List_Node *previous_node = &head;
+    struct Singly_Linked_List_Node *previous_node = &(list->head);
 
     while (search_node.next != NULL)
     {
@@ -88,13 +93,13 @@ void remove_node(int value)
         if (search_node.value == value)
         {
             // If the node is the head element, the head element is re-assigned
-            if (search_node.value == head.value)
+            if (search_node.value == (list->head).value)
             {
                 // search node is assigned to the following node
                 search_node = *(search_node).next;
                 // The value and pointer of the head node are re-assigned
-                head.value = search_node.value;
-                head.next = search_node.next;
+                (list->head).value = search_node.value;
+                (list->head).next = search_node.next;
             }
             // If the node to remove is any other element in the list
             else
@@ -103,12 +108,12 @@ void remove_node(int value)
                 (*previous_node).next = search_node.next;
             }
 
-            printf("\nNode with value %d removed.", value);
+            list->list_size--;
             break;
         }
 
         // If the current node is not the head node, the pointer to the previous element is re-assigned
-        if (search_node.value != head.value)
+        if (search_node.value != (list->head).value)
         {
             previous_node = (*previous_node).next;
         }
--- Example_Files/Singly_Linked_List_Test.c
+++ Example_Files/Singly_Linked_List_Test.c
@@ -1,25 +1,87 @@
+/**
+ ********************************************************************************
+ * @file    Singly_Linked_List_Test.c
+ * @author  Kai Gehry
+ * @date    2025-03-25
+ * @brief
+ ********************************************************************************
+ */
+
+/************************************
+ * INCLUDES
+ ************************************/
+
 #include <stdio.h>
 #include "C:\Users\Kai Gehry\Desktop\C\C_Data_Structures\Data_Strutures\Singly_Linked_List.h"
 
-// Used to test the functions defined in the singly linked list header file
+/************************************
+ * EXTERN VARIABLES
+ ************************************/
+
+/************************************
+ * PRIVATE MACROS AND DEFINES
+ ************************************/
+
+/************************************
+ * PRIVATE TYPEDEFS
+ ************************************/
+
+/************************************
+ * STATIC VARIABLES
+ ************************************/
+
+/************************************
+ * GLOBAL VARIABLES
+ ************************************/
+
+/************************************
+ * STATIC FUNCTION PROTOTYPES
+ ************************************/
+
+/************************************
+ * STATIC FUNCTIONS
+ ************************************/
+
+/************************************
+ * GLOBAL FUNCTIONS
+ ************************************/
 
 int main()
 {
 
-    add_node(1);
-    add_node(2);
-    add_node(3);
-    add_node(4);
+    // Creates a new linked list
+    Singly_Linked_List list1;
 
-    display_linked_list();
-    int size = list_length();
+    // Initializes the size of the linked list to 0
+    initialize_list(&list1);
+    // Displays the contents of the linked list
+    display_linked_list(&list1);
+
+    // Adds items to the list
+    add_node(&list1, 1);
+    add_node(&list1, 2);
+
+    display_linked_list(&list1);
+
+    // Removes an items
+    remove_node(&list1, 2);
+    display_linked_list(&list1);
+
+    // Displays the number of elements in the list
+    int size = list_length(&list1);
     printf("\nSize of list: %d", size);
 
-    int head_elem = return_head_element();
-    printf("\nHead Elem: %d\n", head_elem);
+    // Creates another linked list and adds and removes from it
+    Singly_Linked_List list1;
+    initialize_list(&list1);
 
-    remove_node(2);
-    display_linked_list();
+    add_node(&list1, 3);
+    add_node(&list1, 4);
+
+    display_linked_list(&list1);
+    remove_node(&list1, 4);
+    remove_node(&list1, 3);
+    display_linked_list(&list1);
 
     return 0;
 }
