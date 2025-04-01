902e07b240fc9a266ffa2d6da85d510df6e9ba7c
Kai Gehry <kaigehry@gmail.com>
2025-03-31 20:50:42 -0700

    include satements updated

--- Example_Files/Singly_Linked_List_Test.c
+++ Example_Files/Singly_Linked_List_Test.c
@@ -13,7 +13,7 @@
  * INCLUDES
  ************************************/
 #include <stdio.h>
-#include "C:\Users\Kai Gehry\Desktop\C\C_Data_Structures\Data_Structures\singly_linked_list.h"
+#include "../Data_Structures/Singly_Linked_List.h"
 
 /*!
  * @brief main function used to test the functionality of the singly linked list header file.
