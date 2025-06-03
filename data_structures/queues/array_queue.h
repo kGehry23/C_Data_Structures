8e68a3e9753f2fc9c77f6a3a364b6941608b5983
Kai Gehry <kaigehry@gmail.com>
2025-06-02 23:38:53 -0700

    Initial setup completed for queue header file, and unneeded header file include removed from array_stack.h

---
+++ data_structures/queues/array_queue.h
@@ -0,0 +1,28 @@
+/**
+ ********************************************************************************
+ * @file    array_queue.h
+ * @author  Kai Gehry
+ * @date    2025-06-02
+ *
+ * @brief   Defines the operations on an array based queue.
+ ********************************************************************************
+ */
+
+/************************************
+ * INCLUDES
+ ************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+/************************************
+ * COMPILER DIRECTIVES
+ ************************************/
+// Added for void* to required type conversions
+#pragma GCC diagnostic ignored "-Wint-conversion"
+
+/*!
+ * @brief Struct which represents an array based queue.
+ */
+typedef struct
+{
+} array_queue;
--- data_structures/stacks/array_stack.h
+++ data_structures/stacks/array_stack.h
@@ -13,7 +13,6 @@
  ************************************/
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 /************************************
  * COMPILER DIRECTIVES
