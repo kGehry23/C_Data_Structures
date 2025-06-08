87805d7b18a4aa15b9ffe2df264be0e8c7cddcbd
Kai Gehry <kaigehry@gmail.com>
2025-06-07 22:33:42 -0700

    Comments updated and compiler directive added

--- data_structures/graphs/undirected_graph.h
+++ data_structures/graphs/undirected_graph.h
@@ -17,6 +17,12 @@
 #include <stdbool.h>
 #include "undirected_graph_vertex.h"
 
+/************************************
+ * COMPILER DIRECTIVES
+ ************************************/
+// Added for void* to required type conversions
+#pragma GCC diagnostic ignored "-Wint-conversion"
+
 /*!
  * @brief Struct which represents an undirected graph
  */
@@ -26,33 +32,10 @@ typedef struct
     int num_vertices;
     // Stores the number of edges
     int num_edges;
-    // Hash table used to store vertex ids and associated pointers
+    // Pointer to 2D array to indicate if two vertices are connected
     int **adjacency_matrix;
 
 } undirected_graph;
-
-// /*!
-//  * @brief Adds a vertex to the undirected graph
-//  * @param ud_graph Pointer to an undirected graph
-//  * @param identifier Value to identify the vertex by
-//  * @param vertex_value Value stored by the vertex
-//  * @return None
-//  */
-// void add_vertex(undirected_graph *ud_graph, void *identifier, void *vertex_value)
-// {
-//     // Allocate memory for a new undirected graph node
-//     undirected_graph_vertex *new_vertex = (undirected_graph_vertex *)malloc(sizeof(undirected_graph_vertex));
-
-//     // Assign the specified identifier and value to the new node
-//     new_vertex->identifier = identifier;
-//     new_vertex->value = vertex_value;
-
-//     // Put the identifier and a pointer to the new node in the graph's hash table
-//     put((ud_graph->vertex_data), identifier, new_vertex);
-
-//     // Increment the number of vertices in the graph
-//     ud_graph->num_vertices = ud_graph->num_vertices + 1;
-// }
 
 /*!
  * @brief Adds a vertex to the undirected graph
@@ -61,7 +44,7 @@ typedef struct
  * @param vertex_value Value stored by the vertex
  * @return None
  */
-void add_vertex(undirected_graph *ud_graph)
+void add_vertex(undirected_graph *ud_graph, void *identifier, void *vertex_value)
 {
     // // Allocate memory for a new undirected graph node
     // undirected_graph_vertex *new_vertex = (undirected_graph_vertex *)malloc(sizeof(undirected_graph_vertex));
@@ -81,6 +64,7 @@ void add_vertex(undirected_graph *ud_graph)
 /*!
  * @brief Initializes an undirected graph
  * @param ud_graph Pointer to an undirected graph
+ * @param initial_capacity Row and column dimension of the adjacency matrix
  * @return None
  */
 void initialize_undirected_graph(undirected_graph *ud_graph, int initial_capacity)
@@ -89,13 +73,16 @@ void initialize_undirected_graph(undirected_graph *ud_graph, int initial_capacit
     ud_graph->num_vertices = 0;
     ud_graph->num_edges = 0;
 
+    // Resize the main array to the required size
     ud_graph->adjacency_matrix = (int *)malloc(initial_capacity * sizeof(int));
 
+    // Populate the main array with internal arrays
     for (int i = 0; i < initial_capacity; i++)
     {
         (ud_graph->adjacency_matrix)[i] = (int *)malloc(initial_capacity * sizeof(int));
     }
 
+    // Initialize all elements in the adjacency matrix to 0
     for (int j = 0; j < initial_capacity; j++)
     {
         for (int k = 0; k < initial_capacity; k++)
--- example_files/undirected_graph_test.c
+++ example_files/undirected_graph_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <strings.h>
+#include <stdbool.h>
 #include "../data_structures/graphs/undirected_graph.h"
 
 int main(void)
@@ -8,8 +9,8 @@ int main(void)
 
     initialize_undirected_graph(&ud_graph, 5);
 
-    add_vertex(&ud_graph);
-    add_vertex(&ud_graph);
+    add_vertex(&ud_graph, "A", 1);
+    add_vertex(&ud_graph, "B", 2);
 
     // printf("\n%d", ud_graph.num_vertices);
 
