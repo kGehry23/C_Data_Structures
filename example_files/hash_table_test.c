181258b2964700d3cb9015486f52b01b2e324a63
Kai Gehry <kaigehry@gmail.com>
2025-06-12 23:48:42 -0700

    Header file structure for the hash_table data structure updated

--- example_files/hash_table_test.c
+++ example_files/hash_table_test.c
@@ -16,6 +16,9 @@
 #include <strings.h>
 #include "../data_structures/hash_table/hash_table.h"
 
+/*!
+ * @brief main function used to test the functionality of the hash_table header file.
+ */
 int main(void)
 {
     hash_table table;
