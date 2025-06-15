a8756d982e9c958dbd83829835ef17bfa40befd3
Kai Gehry <kaigehry@gmail.com>
2025-06-15 10:46:37 -0700

    .c file created for unidrected graph header file function implementation

---
+++ data_structures/graphs/directed_graph/directed_graph.c
@@ -0,0 +1,182 @@
+/**
+ ********************************************************************************
+ * @file    directed_graph.c
+ * @author  Kai Gehry
+ * @date    2025-06-15
+ *
+ * @brief   Defines the operations on a directed graph
+ ********************************************************************************
+ */
+
+/************************************
+ * INCLUDES
+ ************************************/
+#include "directed_graph.h"
+
+/************************************
+ * FUNCTION DEFINITIONS
+ ************************************/
+
+/*!
+ * @brief Adds a vertex to the directed graph
+ * @param d_graph Pointer to an directed graph
+ * @param identifier Value to identify the vertex by
+ * @param vertex_value Value stored by the vertex
+ * @return None
+ */
+void add_vertex(directed_graph *d_graph, int identifier, void *vertex_value)
+{
+
+    // Allocate memory for a new directed graph vertex
+    directed_graph_vertex *new_vertex = (directed_graph_vertex *)malloc(sizeof(directed_graph_vertex));
+
+    /*
+      Assign the specified identifier, value stored by the new node, and an associated index into the adjacency
+      matrix.
+    */
+    new_vertex->identifier = identifier;
+    new_vertex->value = vertex_value;
+    new_vertex->adj_index = identifier;
+
+    // Add vertex to the graph
+    d_graph->vertices[identifier] = new_vertex;
+
+    // Increment the number of vertices in the graph
+    d_graph->num_vertices = d_graph->num_vertices + 1;
+}
+
+/*!
+ * @brief Removes and returns the value associated with the specified vertex
+ *        in the undirected graph
+ * @param d_graph Pointer to an directed graph
+ * @param identifier Value to identify the vertex by
+ * @return None
+ */
+void *remove_vertex(directed_graph *d_graph, int identifier)
+{
+    // Pointer to the vertex to be removed
+    void *removed_vertex_value = (d_graph->vertices[identifier])->value;
+
+    // Free memory held by removed vertex
+    free(d_graph->vertices[identifier]);
+    // Set the pointer to the vertex to NULL to avoid dangling pointer
+    d_graph->vertices[identifier] = NULL;
+
+    // Sets all the edges that may exist attached to the node to remove to FALSE in the adjacency matrix
+    for (int i = 0; i < d_graph->adjacency_size; i++)
+    {
+        if ((d_graph->adjacency_matrix)[i][identifier] == TRUE)
+        {
+            (d_graph->adjacency_matrix)[i][identifier] = FALSE;
+            (d_graph->adjacency_matrix)[identifier][i] = FALSE;
+        }
+    }
+
+    // Decrement the counter for the number of vertices
+    d_graph->num_vertices = d_graph->num_vertices - 1;
+
+    return removed_vertex_value;
+}
+
+/*!
+ * @brief Returns the value stored by the specified vertex.
+ * @param d_graph Pointer to an undirected graph
+ * @param identifier Value to identify the vertex by
+ * @return The value stored by the specified vertex
+ */
+void *return_vertex_value(directed_graph *d_graph, int identifier)
+{
+    return (d_graph->vertices[identifier])->value;
+}
+
+/*!
+ * @brief Adds an edge between two vertices in the directed graph
+ * @param d_graph Pointer to a directed graph
+ * @param vertex_id_1 Identifier of the first vertex
+ * @param vertex_id_2 Identifier of the second vertex
+ * @return None
+ */
+void add_edge(directed_graph *d_graph, int vertex_id_1, int vertex_id_2)
+{
+    // Updates the relevant indices in the adjacency matrix to TRUE (1)
+    (d_graph->adjacency_matrix)[vertex_id_1][vertex_id_2] = TRUE;
+    (d_graph->adjacency_matrix)[vertex_id_2][vertex_id_1] = TRUE;
+
+    // Increment the counter for the number of edges
+    d_graph->num_edges = d_graph->num_edges + 1;
+}
+
+/*!
+ * @brief Removes an edge between two vertices in the directed graph
+ * @param ud_graph Pointer to a directed graph
+ * @param vertex_id_1 Identifier of the first vertex
+ * @param vertex_id_2 Identifier of the second vertex
+ * @return None
+ */
+void remove_edge(directed_graph *d_graph, int vertex_id_1, int vertex_id_2)
+{
+    // Updates the relevant indices in the adjacency matrix to FALSE (0)
+    (d_graph->adjacency_matrix)[vertex_id_1][vertex_id_2] = FALSE;
+    (d_graph->adjacency_matrix)[vertex_id_2][vertex_id_1] = FALSE;
+
+    // Decrement the counter for the number of edges
+    d_graph->num_edges = d_graph->num_edges - 1;
+}
+
+/*!
+ * @brief Checks if there is an edge between two vertices
+ * @param d_graph Pointer to a directed graph
+ * @param vertex_id_1 Identifier of the first vertex
+ * @param vertex_id_2 Identifier of the second vertex
+ * @return A boolean value indicating if an edge exists between the specified vertices
+ */
+bool edge_exists(directed_graph *d_graph, int vertex_id_1, int vertex_id_2)
+{
+    // Checks if the specified vertices have an edge between them
+    return ((d_graph->adjacency_matrix)[vertex_id_1][vertex_id_2] == TRUE);
+}
+
+/*!
+ * @brief Returns the number of vertices in the graph
+ * @return An integer representing the number of vertices in the graph
+ */
+int graph_size(directed_graph *d_graph)
+{
+    return d_graph->num_vertices;
+}
+
+/*!
+ * @brief Initializes a directed graph
+ * @param ud_graph Pointer to a directed graph
+ * @param size_upper_bound Row and column dimension of the adjacency matrix
+ * @return None
+ */
+void initialize_undirected_graph(directed_graph *d_graph, int size_upper_bound)
+{
+    // Sets the number of vertices and the number of edges to 0
+    d_graph->num_vertices = 0;
+    d_graph->num_edges = 0;
+
+    // Assign the number of rows and columns to the specified upper bound
+    d_graph->adjacency_size = size_upper_bound;
+
+    d_graph->vertices = (directed_graph_vertex **)malloc(size_upper_bound * sizeof(directed_graph_vertex *));
+
+    // Resize the main array to the required size
+    d_graph->adjacency_matrix = (int *)malloc(size_upper_bound * sizeof(int));
+
+    // Populate the main array with internal arrays
+    for (int i = 0; i < size_upper_bound; i++)
+    {
+        (d_graph->adjacency_matrix)[i] = (int *)malloc(size_upper_bound * sizeof(int));
+    }
+
+    // Initialize all elements in the adjacency matrix to 0
+    for (int j = 0; j < size_upper_bound; j++)
+    {
+        for (int k = 0; k < size_upper_bound; k++)
+        {
+            (d_graph->adjacency_matrix)[j][k] = 0;
+        }
+    }
+}
