75e69e859c8b2ee44579376ba84b52bf8d7b846e
Kai Gehry <kaigehry@gmail.com>
2025-06-24 19:41:28 -0700

    Initial header file setup for n-ary tree implementation

---
+++ data_structures/trees/n_ary_tree.h
@@ -0,0 +1,41 @@
+/**
+ ********************************************************************************
+ * @file    n_ary_tree.h
+ * @author  Kai Gehry
+ * @date    2025-06-24
+ *
+ * @brief   Defines the structure and operations defined on an n-ary tree
+ ********************************************************************************
+ */
+
+#ifndef N_ARY_TREE_H
+#define N_ARY_TREE_H
+
+/************************************
+ * INCLUDES
+ ************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/************************************
+ * TYPEDEFS
+ ************************************/
+
+/*!
+ * @brief Struct which represents an n-ary tree node
+ */
+typedef struct
+{
+
+} n_ary_node;
+
+/*!
+ * @brief Struct which represents an n-ary tree
+ */
+typedef struct
+{
+
+} n_ary_tree;
+
+#endif // N_ARY_TREE_H
