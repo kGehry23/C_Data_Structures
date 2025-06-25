1a90cb9d0be80c0ee31b2d6c75c5b0089ad61f07
Kai Gehry <kaigehry@gmail.com>
2025-06-24 21:20:38 -0700

    Test file created and intial function defintion file created

--- data_structures/trees/n_ary_tree.c
+++ data_structures/trees/n_ary_tree.c
@@ -1,60 +1,20 @@
 /**
  ********************************************************************************
- * @file    n_ary_tree.h
+ * @file    n_ary_tree.c
  * @author  Kai Gehry
  * @date    2025-06-24
  *
- * @brief   Defines the structure and operations defined on an n-ary tree
+ * @brief   Defines the operations on an n-ary tree
  ********************************************************************************
  */
-
-#ifndef N_ARY_TREE_H
-#define N_ARY_TREE_H
 
 /************************************
  * INCLUDES
  ************************************/
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include "../linked_lists/singly_linked_list/singly_linked_list.h"
+#include "n_ary_tree.h"
 
 /************************************
- * TYPEDEFS
- ************************************/
-
-/*!
- * @brief Struct which represents an n-ary tree node
- */
-typedef struct n_ary_node
-{
-    // Maximum number of children per node (Tree order)
-    int max_children;
-    // Value held by the node
-    void *node_value;
-    // Node identifier
-    void *node_identifier;
-    // Array of pointers to child nodes
-    struct n_ary_node **child_nodes;
-
-} n_ary_node;
-
-/*!
- * @brief Struct which represents an n-ary tree
- */
-typedef struct
-{
-    // Tree order. Ex. 2 indicates binary
-    int tree_order;
-    // Number of elements in the tree
-    int tree_size;
-    // Pointer to the root node of the tree
-    n_ary_node *root;
-
-} n_ary_tree;
-
-/************************************
- * FUNCTION PROTOTYPES
+ * FUNCTION DEFINITIONS
  ************************************/
 
 /*!
@@ -63,7 +23,9 @@ typedef struct
  * @param order Tree order
  * @return None
  */
-void initialize_n_ary_tree(n_ary_tree *tree, int order);
+void initialize_n_ary_tree(n_ary_tree *tree, int order)
+{
+}
 
 /*!
  * @brief Adds a node to the tree
@@ -71,7 +33,9 @@ void initialize_n_ary_tree(n_ary_tree *tree, int order);
  * @param value Value held by the node
  * @return None
  */
-void add_tree_node(n_ary_tree *tree, void *value);
+void add_tree_node(n_ary_tree *tree, void *value)
+{
+}
 
 /*!
  * @brief Adds a node to the tree
@@ -79,7 +43,10 @@ void add_tree_node(n_ary_tree *tree, void *value);
  * @param identifier Node identifier
  * @return Returns the value held by the node
  */
-void *remove_tree_node(n_ary_tree *tree, void *identifier);
+void *remove_tree_node(n_ary_tree *tree, void *identifier)
+{
+    return NULL;
+}
 
 /*!
  * @brief Returns a pointer to a n-ary node
@@ -87,7 +54,10 @@ void *remove_tree_node(n_ary_tree *tree, void *identifier);
  * @param identifier Node identifier
  * @return A pointer to an n-ary node
  */
-n_ary_node *find_n_ary_node(n_ary_tree *tree, void *identifier);
+n_ary_node *find_n_ary_node(n_ary_tree *tree, void *identifier)
+{
+    return NULL;
+}
 
 /*!
  * @brief Returns the value held by the specified node
@@ -95,48 +65,67 @@ n_ary_node *find_n_ary_node(n_ary_tree *tree, void *identifier);
  * @param identifier Node identifier
  * @return The value held by the specified node
  */
-void *get_node_value(n_ary_tree *tree, void *identifier);
+void *get_node_value(n_ary_tree *tree, void *identifier)
+{
+    return NULL;
+}
 
 /*!
  * @brief Returns the number of nodes in the tree
  * @param tree Pointer to a tree
  * @return An integer representing the number of nodes in the tree
  */
-int num_nodes(n_ary_tree *tree);
+int num_nodes(n_ary_tree *tree)
+{
+    return 0;
+}
 
 /*!
  * @brief Returns the height of the tree
  * @param tree Pointer to a tree
  * @return An integer representing the height of the tree
  */
-int tree_height(n_ary_tree *tree);
+int tree_height(n_ary_tree *tree)
+{
+    return 0;
+}
 
 /*!
  * @brief Returns if the tree is empty
  * @param tree Pointer to a tree
  * @return A boolean representing if the tree is empty
  */
-bool n_ary_is_empty(n_ary_tree *tree);
+bool n_ary_is_empty(n_ary_tree *tree)
+{
+    return 0;
+}
 
 /*!
  * @brief Returns if the tree is balanced
  * @param tree Pointer to a tree
  * @return A boolean representing if the tree is balanced
  */
-bool n_ary_is_balanced(n_ary_tree *tree);
+bool n_ary_is_balanced(n_ary_tree *tree)
+{
+    return 0;
+}
 
 /*!
  * @brief Returns if the tree is complete
  * @param tree Pointer to a tree
  * @return A boolean representing if the tree is complete
  */
-bool n_ary_is_complete(n_ary_tree *tree);
+bool n_ary_is_complete(n_ary_tree *tree)
+{
+    return 0;
+}
 
 /*!
  * @brief Returns if the tree is full
  * @param tree Pointer to a tree
  * @return A boolean representing if the tree is full
  */
-bool n_ary_is_full(n_ary_tree *tree);
-
-#endif // N_ARY_TREE_H
+bool n_ary_is_full(n_ary_tree *tree)
+{
+    return 0;
+}
---
+++ example_files/n_ary_tree_test.c
@@ -0,0 +1,21 @@
+/**
+ ********************************************************************************
+ * @file    n_ary_tree_test.c
+ * @author  Kai Gehry
+ * @date    2025-06-24
+ *
+ * @brief   Tests the functionality of the operations defined in
+ *          the n_ary_tree header file.
+ ********************************************************************************
+ */
+
+/************************************
+ * INCLUDES
+ ************************************/
+#include <stdio.h>
+#include "../data_structures/trees/n_ary_tree.h"
+
+int main(void)
+{
+    return 0;
+}
