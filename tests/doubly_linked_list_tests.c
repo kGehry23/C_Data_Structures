d242b1e102b89ae89de53583cc30477c8778e276
Kai Gehry <kaigehry@gmail.com>
2025-07-08 23:16:28 -0700

    doubly linked list test file inital setup

---
+++ tests/doubly_linked_list_tests.c
@@ -0,0 +1,19 @@
+/**
+ ********************************************************************************
+ * @file    doubly_linked_list_tests.c
+ * @author  Kai Gehry
+ * @date    2025-07-08
+ *
+ * @brief   Test the operations defined in the doubly_linked_list.c file.
+ ********************************************************************************
+ */
+
+/************************************
+ * INCLUDES
+ ************************************/
+#include <assert.h>
+#include "../data_structures/linked_lists/doubly_linked_list/doubly_linked_list.h"
+
+/************************************
+ * TESTS
+ ************************************/
