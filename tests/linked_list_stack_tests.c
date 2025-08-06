72d9e422b319b622ae0c88dfc80b9d676449afe9
Kai Gehry <kaigehry@gmail.com>
2025-08-05 23:15:51 -0700

    linked list stack test file started

---
+++ tests/linked_list_stack_tests.c
@@ -0,0 +1,19 @@
+/**
+ ********************************************************************************
+ * @file    linked_list_stack_tests.c
+ * @author  Kai Gehry
+ * @date    2025-08-06
+ *
+ * @brief   Test the operations defined in the linked_list_stack.c file.
+ ********************************************************************************
+ */
+
+/************************************
+ * INCLUDES
+ ************************************/
+#include <assert.h>
+#include "../data_structures/stacks/linked_list_stack/linked_list_stack.h"
+
+/************************************
+ * TESTS
+ ************************************/
